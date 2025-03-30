3201c49d7844d20bab6902adcafe03a463941453
mpdsant <michael.santos@embraer.com.br>
2025-03-30 14:15:35 -0300

    create test for the categories dashboard

--- src/main/java/com/example/fc_api/controller/param/CategoriesPostParam.java
+++ src/main/java/com/example/fc_api/controller/param/CategoriesPostParam.java
@@ -1,10 +1,12 @@
 package com.example.fc_api.controller.param;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
+@AllArgsConstructor
 public class CategoriesPostParam {
 
     private Long id;
